76710f12aeed8dabed78a87cdffe4f62f90c6583
이진우 <leejinwoo1126@gmail.com>
2021-11-14 11:35:37 +0900

    211114 기본자료구조 연습예제 추가

---
+++ Java/MyDoubleLinkedList.java
@@ -0,0 +1,133 @@
+﻿public class MyDoubleLinkedList<T> {
+    public Node<T> head = null;
+    public Node<T> tail = null;
+    
+    public class Node<T>{
+        T data;
+        Node<T> prev = null;
+        Node<T> next = null;
+        
+        public Node(T data){
+            this.data = data;
+        }
+    }
+    
+    public void addNode(T data){
+        if(this.head == null){
+            this.head = new Node<T>(data);
+            this.tail = this.head;
+        }else{
+            Node<T> node = this.head;
+            
+            while(node.next != null){
+                node = node.next;
+            } // nexe = null 인 노드 찾음 
+            
+            node.next = new Node<T>(data);
+            node.next.prev = node; // 새로 생성 node.prev = 현재 node; 
+            this.tail = node.next; // 여기 빼먹음 
+        }
+    }
+    
+    public void printAll(){
+        if(this.head != null){
+            Node<T> node = this.head ; 
+            System.out.println(node.data);
+            while(node.next != null){
+                node = node.next;
+                System.out.println(node.data);                
+            }
+        }
+    }
+    //순방향 검색 
+    public T searchFromHead(T isData){
+        if(this.head == null){ // 데이터 없음
+            return null;
+        }else{
+            Node<T> node = this.head;
+            while(node != null){
+                if(node.data == isData){
+                    return node.data;
+                }else{
+                    node = node.next ; // 마지막은 null이니
+                }
+            }
+            return null; // 데이터 없음
+        }
+    }
+    // 역방향 검색
+    public T searchFromTail(T isData){
+        if(this.head == null){
+            return null;
+        }else{
+            Node<T> node = this.tail; 
+            while(node != null){
+                if(node.data == isData){
+                    return node.data;
+                }else{
+                    node = node.prev;   // head까지 search하면 null
+                }
+            }
+            return null;
+        }
+    }
+    
+    // 데이터를 임의 노드 앞에 노드를 추가하는 메서드 추가하기
+    public boolean insertToFront(T existedData, T addData){
+        
+        if(this.head == null){// 데이터가 없는 경우 
+            this.head = new Node<T>(addData);
+            this.tail = this.head;
+            return true;
+        }else if(this.head.data == existedData){//head 앞에 넣는 경우
+            Node<T> newHead = new Node<T>(addData);
+            newHead.next = this.head; // prev 초기화시 null 
+            this.head = newHead;
+            return true;
+        }else{ // 노드 중간에 삽입하는 경우
+            Node<T> node = this.head;
+            while(node != null){
+                if(node.data == existedData){
+                    Node<T> nodePrev = node.prev;
+                    
+                    nodePrev.next = new Node<T>(addData); //앞에 노드의 next에 
+                    nodePrev.next.next = node;
+
+                    nodePrev.next.prev = nodePrev; //신규생성node의 prev
+                    node.prev = nodePrev.next; // 검색한 node에 prev는 신규노드
+                    return true;
+                }else{
+                    node = node.next;
+                }
+            }
+            return false; // 데이터가 존재하지 않는 경우
+        }
+        
+        
+    }
+    
+    
+    public static void main(String[] args) {
+        MyDoubleLinkedList<Integer> MyLinkedList = new MyDoubleLinkedList<Integer>();
+
+        MyLinkedList.addNode(1);
+        MyLinkedList.addNode(2);
+        MyLinkedList.addNode(3);
+        MyLinkedList.addNode(4);
+        MyLinkedList.addNode(5);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
+
+        MyLinkedList.insertToFront(3, 2);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
+
+        MyLinkedList.insertToFront(6, 2);
+        MyLinkedList.insertToFront(1, 0);
+        MyLinkedList.printAll();
+        System.out.println("----------------");
+
+        MyLinkedList.addNode(6);
+        MyLinkedList.printAll();
+    }
+}
---
+++ Java/MySingleLinkedList.java
@@ -0,0 +1,108 @@
+﻿public class MySingleLinkedList<T> {
+
+    // head 부터 순차적으로 노드 검색
+    public Node<T> head = null;
+    
+    // 생성자
+    public class Node<T>{
+        T data;
+        Node<T> next = null;
+        //생성자
+        public Node(T data){
+            this.data = data;
+        }
+    }
+    
+    //노드 추가 
+    public void addNode(T data){
+        if(head == null){
+            head = new Node<T>(data);
+        }else{
+            Node<T> node = this.head;
+            while(node.next != null){ // 포인터 이동 , 끝의 노드까지 가도록 만듦
+                node = node.next;
+            }
+            node.next = new Node<T>(data);
+        }
+    }
+    
+    // 전체 노드 출력
+    public void printAll(){
+        if(head != null){
+            Node<T> node = this.head;
+            System.out.println(node.data);
+            while(node.next != null){
+                node = node.next; 
+                System.out.println(node.data);
+            }
+        }
+    }
+    
+    //해당 데이터 가진 노드 리턴 
+    public Node<T> search(T data){
+        if(this.head == null){
+            return null;
+        }else{
+            Node<T> node = this.head; // head부터 순차적으로 검색
+            while(node != null){
+                if(node.data == data){
+                    return node;
+                }
+                node = node.next;
+            }
+            return null;  // 전체 검색했는데 없다면 
+        }
+    }
+    
+    // 중간에 노드 삽입 
+    public void addNodeInside(T data, T isData){
+        Node<T> searchedNode = this.search(isData);
+        
+        if(searchedNode == null){ // 찾는 데이터가 없으면 LinkedList 맨 뒤에 삽입
+            this.addNode(data); 
+        }else{
+            Node<T> nextNode = searchedNode.next; // 기존에 가르키던 next
+            searchedNode.next = new Node<T>(data); // 새로 삽입되는 노드 
+            searchedNode.next.next = nextNode;     
+        }
+    }
+    
+    // 노드 삭제 ( Integer는 Wrapper클래스 )
+    public boolean delNode(T isData){
+        if(this.head == null){ // 데이터 없음 
+            return false; 
+        }else{
+            Node<T> node = this.head;
+            
+            if(node.data == isData){ // 헤드인 경우 
+                this.head = this.head.next;
+                return true;
+            }else{
+               while(node.next != null){
+                    if(node.next.data == isData){
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+               }//end while 
+                
+                return false; 
+            }// end else
+            
+            
+        }// end else 
+    }// end delNode
+    
+    public static void main(String[] args) {
+        MySingleLinkedList<Integer> MyLinkedList = new MySingleLinkedList<Integer>();
+
+        MyLinkedList.addNode(1);
+        MyLinkedList.addNode(2);
+        MyLinkedList.addNode(3);
+        MyLinkedList.addNode(4);
+        MyLinkedList.addNode(5);
+
+        MyLinkedList.printAll();
+
+    }
+}
